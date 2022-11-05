0f4c62cab7ee2f473923707ee3ef34843b3f325c
azharluthfi14 <35468624+azharluthfi14@users.noreply.github.com>
2022-11-05 14:38:42 +0700

    Add to repo

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function App() {
+  return (
+    <div className="layout">
+      <h1 className="font-bold text-2xl text-gray-900">Hello world</h1>
+    </div>
+  );
+}
